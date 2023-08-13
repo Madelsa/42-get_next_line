0c3486071025f0470e2ccdd440fb1dc4e835370f
Madelsa <126403847+Madelsa@users.noreply.github.com>
2023-08-13 06:39:14 -0700

    Updates...

--- get_next_line.c
+++ get_next_line.c
@@ -3,45 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mabdelsa <mabdelsa@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mahmoud <mahmoud@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 13:10:15 by mabdelsa          #+#    #+#             */
-/*   Updated: 2023/08/12 17:11:48 by mabdelsa         ###   ########.fr       */
+/*   Updated: 2023/08/13 06:37:40 by mahmoud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-// char	*read_buffer(int fd, char *stored)
-// {
-// 	char	*buff;
-// 	int		chars_read;
+char	*read_buffer(int fd, char *stored)
+{
+	char	*buff;
+	int		chars_read;
 
-// 	buff = malloc(sizeof(char) * BUFFER_SIZE + 1);
-// 	if (buff == NULL)
-// 		return (NULL);
-// 	while (ft_strchr(stored, '\n') == NULL)
-// 	{
-// 		chars_read = read(fd, buff, BUFFER_SIZE);
-// 		if (chars_read == -1)
-// 			return (free(buff), free(stored), NULL);
-// 		if (chars_read == 0)
-// 			return (free(buff), NULL);
-// 		buff[chars_read] = '\0';
-// 		stored = ft_strjoin(stored, buff);
-// 	}
-// 	return (free(buff), stored);
-// }
+	buff = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (buff == NULL)
+		return (NULL);
+	chars_read = 1;
+	while (chars_read > 0 && ft_strchr(stored, '\n') == NULL)
+	{
+		chars_read = read(fd, buff, BUFFER_SIZE);
+		if (chars_read == -1 || (chars_read == 0 && stored == NULL))
+			return (free(buff), NULL);
+		buff[chars_read] = '\0';
+		stored = ft_strjoin(stored, buff);
+	}
+	return (free(buff), stored);
+}
+
+
 char	*filter_stored(char *stored)
 {
 	int		i;
 	char	*str;
 
 	i = 0;
-	// if (stored[0] == '\0')
-	// 	return (free(stored), NULL);
 	if (stored[0] == '\0')
-		return (NULL);
+		return (free(stored), NULL);
 	while (stored[i] != '\0' && stored[i] != '\n')
 		i++;
 	if (stored[i] == '\n' || stored[i] == '\0')
@@ -59,30 +58,12 @@ char	*update_stored(char *stored)
 	i = 0;
 	while (stored[i] != '\0' && stored[i] != '\n')
 		i++;
+	if (stored[i] == '\0')
+		return (free(stored), NULL);
 	str = ft_strdup(stored + i + 1);
 	return (free(stored), str);
 }
 
-char	*read_buffer(int fd, char *stored)
-{
-	char	*buff;
-	int		chars_read;
-
-	buff = malloc(sizeof(char) * BUFFER_SIZE + 1);
-	if (buff == NULL)
-		return (NULL);
-	while (ft_strchr(stored, '\n') == NULL)
-	{
-		chars_read = read(fd, buff, BUFFER_SIZE);
-		if (chars_read == -1)
-			return (free(buff), free(stored), NULL);
-		if (chars_read == 0)
-			return (free(buff), free(stored), NULL);
-		buff[chars_read] = '\0';
-		stored = ft_strjoin(stored, buff);
-	}
-	return (free(buff), stored);
-}
 
 char	*get_next_line(int fd)
 {
