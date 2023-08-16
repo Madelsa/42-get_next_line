00cddc1e21352e5009338d1d4a039b2c62dd2f18
Madelsa <126403847+Madelsa@users.noreply.github.com>
2023-08-16 03:08:57 -0700

    Updates

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mabdelsa <mabdelsa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 13:10:15 by mabdelsa          #+#    #+#             */
-/*   Updated: 2023/08/15 16:07:29 by mabdelsa         ###   ########.fr       */
+/*   Updated: 2023/08/16 14:06:24 by mabdelsa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ char	*read_buffer(int fd, char *stored)
 	char	*buff;
 	int		chars_read;
 
-	buff = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	buff = malloc(sizeof(char) * (size_t)BUFFER_SIZE + 1);
 	if (buff == NULL)
 		return (NULL);
 	chars_read = 1;
 	while (chars_read > 0 && ft_strchr(stored, '\n') == NULL)
 	{
-		chars_read = read(fd, buff, BUFFER_SIZE);
+		chars_read = read(fd, buff, (size_t)BUFFER_SIZE);
 		if (chars_read == -1 || (chars_read == 0 && (stored == NULL
 					|| stored[0] == '\0')))
 			return (free(buff), free(stored), NULL);
@@ -69,7 +69,7 @@ char	*get_next_line(int fd)
 	static char	*stored = NULL;
 	char		*line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE > 2147483646)
+	if (fd < 0 || (size_t)BUFFER_SIZE <= 0 || (size_t)BUFFER_SIZE > 4294967295)
 		return (NULL);
 	stored = read_buffer(fd, stored);
 	if (stored == NULL)
@@ -83,8 +83,8 @@ char	*get_next_line(int fd)
 
 // int	main(void)
 // {
-// 	int		fd;
-// 	char	*line;
+// 	int fd;
+// 	char *line;
 
 // 	fd = open("file.txt", O_RDONLY);
 // 	line = get_next_line(fd);
